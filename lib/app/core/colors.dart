c4ad69223734b7974ad106423c25871935ba6365
Alexandre Campos <Alexandre.Campos@AB-inbev.com>
2022-09-26 22:28:51 -0300

    creating home mock

---
+++ lib/app/core/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class VColors {
+  static const primary_color = Color(0xff1F1D36);
+  static const secondary_color = Color(0xffE9A6A6);
+  static const white = Color(0xffffffff);
+}

b2a150776c5b43ed9fa5ddcea2f7c1a259a0d4d6
Alexandre Campos <Alexandre.Campos@AB-inbev.com>
2022-10-05 09:10:29 -0300

    removing dart_tools from git

--- lib/app/search/search_page.dart
+++ lib/app/search/search_page.dart
@@ -29,20 +29,30 @@ class _SearchPageState extends State<SearchPage> {
             ),
           ),
           SizedBox(height: 8),
-          ListView.separated(
-            itemCount: 3,
-            separatorBuilder: (context, snapshot) {
-              return FilterChip(
-                label: 'Best Results',
-                isSelected: true,
-              );
-            },
-            itemBuilder: (context, snapshot) {
-              return FilterChip(
-                label: 'Best Results',
-                isSelected: true,
-              );
-            },
+          SizedBox(
+            height: 50,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: filtersList.length,
+              separatorBuilder: (context, snapshot) {
+                return SizedBox(
+                  width: 8,
+                );
+              },
+              itemBuilder: (context, index) {
+                return FilterChip(
+                  label: filtersList[index],
+                  color: selectedFilter == index
+                      ? VColors.secondary
+                      : Colors.transparent,
+                  onTap: () {
+                    setState(() {
+                      selectedFilter = index;
+                    });
+                  },
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -50,24 +60,26 @@ class _SearchPageState extends State<SearchPage> {
   }
 }
 
+final filtersList = ['Best results', 'Albums', 'Artists'];
+
 class FilterChip extends StatelessWidget {
   final String label;
-  final bool isSelected;
-  final Function? onTap;
+  final Color color;
+  final VoidCallback onTap;
   const FilterChip({
     Key? key,
     required this.label,
-    required this.isSelected,
-    this.onTap,
+    required this.onTap,
+    required this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => onTap, //TODO check this
+      onTap: onTap, //TODO ANIMATE IT
       child: Chip(
         label: Text(label),
-        backgroundColor: isSelected ? VColors.secondary : Colors.transparent,
+        backgroundColor: color,
       ),
     );
   }
